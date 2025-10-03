9dd6d2bffc4096506fab4992958daddddcebb864
Ballai Péter Lóránt <ballaipeterlorant@gmail.com>
2025-10-03 16:02:39 +0200

    Finished week4 C# homework

---
+++ semester1/C#/week4/HF_3/div_7_8/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace div_7_8;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int num1;
+        int num2;
+
+        do
+        {
+            System.Console.Write("Please input an integer: ");
+        } while (!Int32.TryParse(Console.ReadLine(), out num1));
+
+        do
+        {
+            System.Console.Write("Please input the second integer: ");
+        } while (!Int32.TryParse(Console.ReadLine(), out num2));
+
+        for (int i = Math.Min(num1, num2); i <= Math.Max(num1, num2); i++)
+        {
+            if (i % 7 == 0)
+            {
+                System.Console.WriteLine($"{i} is divisible by 7 ");
+            }
+            if (i % 8 == 0)
+            {
+                System.Console.WriteLine($"{i} is divisible by 8 ");
+            }
+        }
+    }
+}
