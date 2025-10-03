82bd905798f0706ef41d796b54c18329176307ea
Ballai Péter Lóránt <ballaipeterlorant@gmail.com>
2025-10-03 11:13:20 +0200

    added week4 C# assignments

---
+++ semester1/C#/week4/vector_addition/vector_addition_prog/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace vector_addition_prog;
+
+class Program
+{
+    private struct Vector()
+    {
+        public double X;
+        public double Y;
+
+        public void input()
+        {
+            Console.WriteLine("Input a vector: (x y)");
+            string s = Console.ReadLine();
+            string[] array = s.Split(' ');
+            if (array.Length != 2)
+            {
+                return;
+            }
+
+            X = double.Parse(array[0]);
+            Y = double.Parse(array[1]);
+        }
+
+        public override string ToString()
+        {
+            return $"X: {X} Y: {Y}";
+        }  
+    }
+
+    static void Main(string[] args)
+    {
+        Vector v1 = new Vector();
+        Vector v2 = new Vector();
+        v1.input();
+        v2.input();
+
+        Vector res = new Vector();
+        res.X = v1.X + v2.X;
+        res.Y = v1.Y + v2.Y;
+
+        System.Console.WriteLine($"Result: {res}");
+        
+    }
+}
