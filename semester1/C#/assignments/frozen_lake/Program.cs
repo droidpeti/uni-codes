c93d01600593858258ce8eff1577dbf87028c551
Peter Lorant Ballai <ballaipeterlorant@gmail.com>
2025-10-07 02:11:04 +0200

    finished the program of the first C# assignment

---
+++ semester1/C#/assignments/frozen_lake/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace frozen_lake;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int count = Int32.Parse(Console.ReadLine());
+        int[] data = new int[count];
+        for(int i = 0; i < count; i++){
+            data[i] = Int32.Parse(Console.ReadLine());
+        }
+
+        int frozenDaysCount = data.Where(d => d > 0).Count();
+
+        System.Console.WriteLine(frozenDaysCount);        
+    }
+}
