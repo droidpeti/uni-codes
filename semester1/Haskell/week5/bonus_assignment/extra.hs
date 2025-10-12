d6d59d1ca98e969b24fb744011a9e2e95e66ed30
Peter Lorant Ballai <ballaipeterlorant@gmail.com>
2025-10-12 17:14:42 +0200

    Finished bonus week5 haskell assignments

--- semester1/Haskell/week5/bonus_assignment/extra.hs
+++ semester1/Haskell/week5/bonus_assignment/extra.hs
@@ -2,5 +2,15 @@ module Extra where
 
 closeSection :: Int -> Int -> (Int, Int)
 closeSection x y
- | x == y || ((max x y) -1) == (min x y)
- | otherwise closeSection 
+ | x == y || ((max x y) -1) == (min x y) = (x,y)
+ | otherwise = closeSection ((min x y) + 1) ((max x y) -1)
+
+multiply :: Integer -> Integer -> Integer
+multiply x y
+ | y <= 0 = 0
+ | otherwise = x + multiply x (y -1)
+
+mountain :: Integer -> [Integer]
+mountain x
+ | x <= 0 = []
+ | otherwise = [1..x] ++ (reverse [1..(x-1)])
