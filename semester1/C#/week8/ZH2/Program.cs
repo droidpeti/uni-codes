5ddd525c652bc0e847c4eb2b4f5b434522140771
Ballai Péter Lóránt <ballaipeterlorant@gmail.com>
2025-11-11 20:06:06 +0100

    Added week8 C# ZH

---
+++ semester1/C#/week8/ZH2/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ZH2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Please input a decimal number representing the starting value: ");
+        double start = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Please input a decimal number representing where we stop: ");
+        double end = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Please input a decimal number representing the size of the steps we take: ");
+        double step = Convert.ToDouble(Console.ReadLine());
+
+        for(double i = start; i <= end; i+=step)
+        {
+            Console.WriteLine($"{i}^2 = {i*i}");
+        }
+    }
+}
