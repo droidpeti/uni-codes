d095a36b85b6de585c155654621165faa2142703
Ballai Péter Lóránt <ballaipeterlorant@gmail.com>
2025-11-14 11:01:59 +0100

    Added week9 C# Assignments

---
+++ semester1/C#/week9/LibraryBooks/Program.cs
@@ -0,0 +1,189 @@
+﻿namespace LibraryBooks;
+
+class Program
+{
+    struct Book
+    {
+        public string Author;
+        public string Title;
+        public int Count;
+        public int RentCount;
+    }
+
+    static Book[] ReadArray()
+    {
+        int n = 0;
+        int.TryParse(Console.ReadLine(), out n);
+        Book[] books = new Book[n];
+
+        for(int i = 0; i < n; i++)
+        {
+            Book b = new Book();
+            b.Author = Console.ReadLine();
+            b.Title = Console.ReadLine();
+            b.Count = int.Parse(Console.ReadLine());
+            b.RentCount = int.Parse(Console.ReadLine());
+
+            books[i] = b;
+        }
+
+        return books;
+    }
+
+    static List<Book> ReadList()
+    {
+        int n = 0;
+        int.TryParse(Console.ReadLine(), out n);
+        List<Book> books = new List<Book>();
+
+        for(int i = 0; i < n; i++)
+        {
+            Book b = new Book();
+            b.Author = Console.ReadLine();
+            b.Title = Console.ReadLine();
+            b.Count = int.Parse(Console.ReadLine());
+            b.RentCount = int.Parse(Console.ReadLine());
+
+            books.Add(b);
+        }
+
+        return books;
+    }
+
+    static void ReadArrayOut(out Book[] books)
+    {
+        int n = 0;
+        int.TryParse(Console.ReadLine(), out n);
+        books = new Book[n];
+
+        for(int i = 0; i < n; i++)
+        {
+            Book b = new Book();
+            b.Author = Console.ReadLine();
+            b.Title = Console.ReadLine();
+            b.Count = int.Parse(Console.ReadLine());
+            b.RentCount = int.Parse(Console.ReadLine());
+
+            books[i] = b;
+        }
+    }
+
+    static List<Book> FilterBooksArrayList(Book[] books)
+    {
+        List<Book> filtered = new List<Book>();
+
+        for(int i = 0; i < books.Length; i++)
+        {
+            if(books[i].Count - books[i].RentCount <= 2)
+            {
+                filtered.Add(books[i]);
+            }
+        }
+
+        return filtered;
+    }
+
+    static List<Book> FilterBooksListList(List<Book> books)
+    {
+        List<Book> filtered = new List<Book>();
+
+        for(int i = 0; i < books.Count; i++)
+        {
+            if(books[i].Count - books[i].RentCount <= 2)
+            {
+                filtered.Add(books[i]);
+            }
+        }
+
+        return filtered;
+    }
+
+    static Book[] BookAppend(Book[] books, Book book)
+    {
+        Book[] n = new Book[books.Length+1];
+
+        for(int i = 0; i < books.Length; i++)
+        {
+            n[i] = books[i];
+        }
+
+        n[n.Length-1] = book;
+
+        return n;
+    }
+
+    static Book[] FilterBooksArrayArray(Book[] books)
+    {
+        Book[] filtered = new Book[0];
+
+        for(int i = 0; i < books.Length; i++)
+        {
+            if(books[i].Count - books[i].RentCount <= 2)
+            {
+                filtered = BookAppend(filtered, books[i]);
+            }
+        }
+
+        return filtered;
+    }
+
+    static void FilterBooksArrayListOut(Book[] books, out List<Book> filtered)
+    {
+        filtered = new List<Book>();
+
+        for(int i = 0; i < books.Length; i++)
+        {
+            if(books[i].Count - books[i].RentCount <= 2)
+            {
+                filtered.Add(books[i]);
+            }
+        }
+    }
+
+    static void WriteList(List<Book> books)
+    {
+        for(int i = 0; i < books.Count; i++)
+        {
+            System.Console.WriteLine(books[i].Title);
+        }
+    }
+
+    static void WriteArray(Book[] books)
+    {
+        for(int i = 0; i < books.Length; i++)
+        {
+            System.Console.WriteLine(books[i].Title);
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        // V1
+        /*
+        Book[] books = ReadArray();
+        List<Book> filtered = FilterBooksArrayList(books);
+        WriteList(filtered);
+        */
+
+        // V2
+        /*
+        List<Book> books = ReadList();
+        List<Book> filtered = FilterBooksListList(books);
+        WriteList(filtered);
+        */
+
+        // V3
+        /*
+        Book[] books = ReadArray();
+        Book[] filtered = FilterBooksArrayArray(books);
+        WriteArray(filtered);
+        */
+
+        //V4
+        Book[] books = new Book[0];
+        ReadArrayOut(out books);
+        List<Book> filtered;
+        FilterBooksArrayListOut(books, out filtered);
+        WriteList(filtered);
+    }
+}
---
+++ semester1/C#/week9/ZH_6/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ZH_6;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Please input the starting number: ");
+        double start;
+        Double.TryParse(Console.ReadLine(), out start);
+
+        Console.Write("Please input the number where we stop: ");
+        double stop;
+        Double.TryParse(Console.ReadLine(), out stop);
+
+        Console.Write("Please input the number we step by each time: ");
+        double step;
+        Double.TryParse(Console.ReadLine(), out step);
+
+        for(double i = start; i <= stop; i += step)
+        {
+            System.Console.WriteLine($"{i}^2 = {i*i}");
+        }
+    }
+}
