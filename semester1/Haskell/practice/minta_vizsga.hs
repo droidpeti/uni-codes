744e6264aec395319227e205a3c51f4bb38fc66b
Peter Lorant Ballai <ballaipeterlorant@gmail.com>
2025-12-09 19:50:12 +0100

    Finished Haskell Practice Exam

--- semester1/Haskell/practice/minta_vizsga.hs
+++ semester1/Haskell/practice/minta_vizsga.hs
@@ -61,3 +61,38 @@ combineListsIf _ _ _ [] = []
 combineListsIf pred func (x:xs) (y:ys)
  | pred x y = func x y : combineListsIf pred func xs ys
  | otherwise = combineListsIf pred func xs ys
+
+data Line = Tram Integer [String]
+          | Bus Integer [String]
+          deriving (Eq, Show)
+
+whichBusStop :: String -> [Line] -> [Integer]
+whichBusStop _ [] = []
+whichBusStop station (l:ls)
+ | elem station (getStops l) && getVehicleType l == "Bus" = getNumber l : whichBusStop station ls
+ | otherwise = whichBusStop station ls
+
+getStops :: Line -> [String]
+getStops (Tram _ stops) = stops
+getStops (Bus _ stops)  = stops
+
+getNumber :: Line -> Integer
+getNumber (Tram number _) = number
+getNumber (Bus number _) = number
+
+getVehicleType :: Line -> String
+getVehicleType (Tram _ _) = "Tram"
+getVehicleType (Bus _ _)  = "Bus"
+
+isReservable :: Int -> String -> Bool
+isReservable 0 _ = True
+isReservable _ "" = False
+isReservable count seats = isReservable' 0 count seats
+   where
+     isReservable' :: Int -> Int -> String -> Bool
+     isReservable' counter target _ 
+      | counter == target = True
+     isReservable' _ _ "" = False
+     isReservable' counter target (s:ls)
+      | s == 'x'  = isReservable' (counter + 1) target ls
+      | otherwise = isReservable' 0 target ls
